7488920b479537366a51debc4f0f1bf134c5d769
PietroDomi <pietro.dominietto@gmail.com>
2020-06-21 17:21:25 +0200

    Update data_expl.py

--- old/data_expl.py
+++ old/data_expl.py
@@ -5,25 +5,28 @@ from gensim.corpora import Dictionary
 
 average = 0
 i = 0
-# for doc in os.listdir("data/converted/"):
-#     if int(doc[1:5]) >= 2000:
-#         i += 1
-#         text = word_tokenize(open("data/converted/"+doc,"r",encoding='utf-8').read(), language='italian')
-#         average += len(text)
+for doc in os.listdir("data/converted/"):
+    if int(doc[1:5]) >= 2000:
+        i += 1
+        text = word_tokenize(open("data/converted/"+doc,"r",encoding='utf-8').read(), language='italian')
+        average += len(text)
 
 print(average/i, i)
 
-av = 0
+av1 = 0
+av2 = 0
 i = 0
 for dic in os.listdir("data/.preprocessed"):
-    if dic[0] == 's':
-        year = pickle.load(open("data/.preprocessed/"+dic,"rb"))
-        i += len(year)
-        print(len(year))
-        for doc in year:
-            av += len(doc)
+    year = pickle.load(open("data/.preprocessed/"+dic,"rb"))
+    i += len(year)
+    print(len(year))
+    for doc in year:
+        if dic[0] == 's':
+            av1 += len(doc)
+        else:
+            av2 += len(doc)
 
-print(av/i, i)
+print(av1/i, av2/i, i)
 
 nlp = Italian()
 
